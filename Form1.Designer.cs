a3adde8a97ffefb1922f9d47a71cf635cd9f07e0
User-Admin <drobinson51@qub.ac.uk>
2024-01-31 14:20:58 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace Calculator
+{
+    partial class calculatorApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNumber7 = new System.Windows.Forms.Button();
+            this.btnNumber8 = new System.Windows.Forms.Button();
+            this.btnNumber9 = new System.Windows.Forms.Button();
+            this.btnSubtractionSymbol = new System.Windows.Forms.Button();
+            this.btnNumber4 = new System.Windows.Forms.Button();
+            this.btnNumber5 = new System.Windows.Forms.Button();
+            this.btnNumber6 = new System.Windows.Forms.Button();
+            this.btnNumber3 = new System.Windows.Forms.Button();
+            this.btnNumber2 = new System.Windows.Forms.Button();
+            this.btnNumber0 = new System.Windows.Forms.Button();
+            this.btnEqualsSymbol = new System.Windows.Forms.Button();
+            this.btnPlusSymbol = new System.Windows.Forms.Button();
+            this.btnMultiplicationSymbol = new System.Windows.Forms.Button();
+            this.btnNumber1 = new System.Windows.Forms.Button();
+            this.btnDivisionSymbol = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.btnClearEntry = new System.Windows.Forms.Button();
+            this.btnDecimalPoint = new System.Windows.Forms.Button();
+            this.textboxResult = new System.Windows.Forms.TextBox();
+            this.currentSum = new System.Windows.Forms.Label();
+            this.btnNegativeOrPositiveToggle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNumber7
+            // 
+            this.btnNumber7.Location = new System.Drawing.Point(8, 106);
+            this.btnNumber7.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNumber7.Name = "btnNumber7";
+            this.btnNumber7.Size = new System.Drawing.Size(100, 92);
+            this.btnNumber7.TabIndex = 0;
+            this.btnNumber7.Text = "7";
+            this.btnNumber7.UseVisualStyleBackColor = true;
+            this.btnNumber7.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // btnNumber8
+            // 
+            this.btnNumber8.Location = new System.Drawing.Point(120, 106);
+            this.btnNumber8.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNumber8.Name = "btnNumber8";
+            this.btnNumber8.Size = new System.Drawing.Size(100, 92);
+            this.btnNumber8.TabIndex = 1;
+            this.btnNumber8.Text = "8";
+            this.btnNumber8.UseVisualStyleBackColor = true;
+            this.btnNumber8.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // btnNumber9
+            // 
+            this.btnNumber9.Location = new System.Drawing.Point(232, 106);
+            this.btnNumber9.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNumber9.Name = "btnNumber9";
+            this.btnNumber9.Size = new System.Drawing.Size(100, 92);
+            this.btnNumber9.TabIndex = 2;
+            this.btnNumber9.Text = "9";
+            this.btnNumber9.UseVisualStyleBackColor = true;
+            this.btnNumber9.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // btnSubtractionSymbol
+            // 
+            this.btnSubtractionSymbol.Location = new System.Drawing.Point(344, 314);
+            this.btnSubtractionSymbol.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSubtractionSymbol.Name = "btnSubtractionSymbol";
+            this.btnSubtractionSymbol.Size = new System.Drawing.Size(100, 92);
+            this.btnSubtractionSymbol.TabIndex = 3;
+            this.btnSubtractionSymbol.Text = "-";
+            this.btnSubtractionSymbol.UseVisualStyleBackColor = true;
+            this.btnSubtractionSymbol.Click += new System.EventHandler(this.BtnOperationClick);
+            // 
+            // btnNumber4
+            // 
+            this.btnNumber4.Location = new System.Drawing.Point(8, 210);
+            this.btnNumber4.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNumber4.Name = "btnNumber4";
+            this.btnNumber4.Size = new System.Drawing.Size(100, 92);
+            this.btnNumber4.TabIndex = 4;
+            this.btnNumber4.Text = "4";
+            this.btnNumber4.UseVisualStyleBackColor = true;
+            this.btnNumber4.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // btnNumber5
+            // 
+            this.btnNumber5.Location = new System.Drawing.Point(120, 210);
+            this.btnNumber5.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNumber5.Name = "btnNumber5";
+            this.btnNumber5.Size = new System.Drawing.Size(100, 92);
+            this.btnNumber5.TabIndex = 5;
+            this.btnNumber5.Text = "5";
+            this.btnNumber5.UseVisualStyleBackColor = true;
+            this.btnNumber5.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // btnNumber6
+            // 
+            this.btnNumber6.Location = new System.Drawing.Point(232, 210);
+            this.btnNumber6.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNumber6.Name = "btnNumber6";
+            this.btnNumber6.Size = new System.Drawing.Size(100, 92);
+            this.btnNumber6.TabIndex = 6;
+            this.btnNumber6.Text = "6";
+            this.btnNumber6.UseVisualStyleBackColor = true;
+            this.btnNumber6.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // btnNumber3
+            // 
+            this.btnNumber3.Location = new System.Drawing.Point(232, 314);
+            this.btnNumber3.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNumber3.Name = "btnNumber3";
+            this.btnNumber3.Size = new System.Drawing.Size(100, 92);
+            this.btnNumber3.TabIndex = 7;
+            this.btnNumber3.Text = "3";
+            this.btnNumber3.UseVisualStyleBackColor = true;
+            this.btnNumber3.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // btnNumber2
+            // 
+            this.btnNumber2.Location = new System.Drawing.Point(120, 314);
+            this.btnNumber2.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNumber2.Name = "btnNumber2";
+            this.btnNumber2.Size = new System.Drawing.Size(100, 92);
+            this.btnNumber2.TabIndex = 8;
+            this.btnNumber2.Text = "2";
+            this.btnNumber2.UseVisualStyleBackColor = true;
+            this.btnNumber2.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // btnNumber0
+            // 
+            this.btnNumber0.Location = new System.Drawing.Point(8, 526);
+            this.btnNumber0.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNumber0.Name = "btnNumber0";
+            this.btnNumber0.Size = new System.Drawing.Size(212, 92);
+            this.btnNumber0.TabIndex = 9;
+            this.btnNumber0.Text = "0";
+            this.btnNumber0.UseVisualStyleBackColor = true;
+            this.btnNumber0.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // btnEqualsSymbol
+            // 
+            this.btnEqualsSymbol.Location = new System.Drawing.Point(456, 314);
+            this.btnEqualsSymbol.Margin = new System.Windows.Forms.Padding(6);
+            this.btnEqualsSymbol.Name = "btnEqualsSymbol";
+            this.btnEqualsSymbol.Size = new System.Drawing.Size(100, 196);
+            this.btnEqualsSymbol.TabIndex = 10;
+            this.btnEqualsSymbol.Text = "=";
+            this.btnEqualsSymbol.UseVisualStyleBackColor = true;
+            this.btnEqualsSymbol.Click += new System.EventHandler(this.BtnEqualsClick);
+            // 
+            // btnPlusSymbol
+            // 
+            this.btnPlusSymbol.Location = new System.Drawing.Point(344, 418);
+            this.btnPlusSymbol.Margin = new System.Windows.Forms.Padding(6);
+            this.btnPlusSymbol.Name = "btnPlusSymbol";
+            this.btnPlusSymbol.Size = new System.Drawing.Size(100, 92);
+            this.btnPlusSymbol.TabIndex = 11;
+            this.btnPlusSymbol.Text = "+";
+            this.btnPlusSymbol.UseVisualStyleBackColor = true;
+            this.btnPlusSymbol.Click += new System.EventHandler(this.BtnOperationClick);
+            // 
+            // btnMultiplicationSymbol
+            // 
+            this.btnMultiplicationSymbol.Location = new System.Drawing.Point(344, 210);
+            this.btnMultiplicationSymbol.Margin = new System.Windows.Forms.Padding(6);
+            this.btnMultiplicationSymbol.Name = "btnMultiplicationSymbol";
+            this.btnMultiplicationSymbol.Size = new System.Drawing.Size(100, 92);
+            this.btnMultiplicationSymbol.TabIndex = 12;
+            this.btnMultiplicationSymbol.Text = "X";
+            this.btnMultiplicationSymbol.UseVisualStyleBackColor = true;
+            this.btnMultiplicationSymbol.Click += new System.EventHandler(this.BtnOperationClick);
+            // 
+            // btnNumber1
+            // 
+            this.btnNumber1.Location = new System.Drawing.Point(8, 314);
+            this.btnNumber1.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNumber1.Name = "btnNumber1";
+            this.btnNumber1.Size = new System.Drawing.Size(100, 92);
+            this.btnNumber1.TabIndex = 13;
+            this.btnNumber1.Text = "1";
+            this.btnNumber1.UseVisualStyleBackColor = true;
+            this.btnNumber1.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // btnDivisionSymbol
+            // 
+            this.btnDivisionSymbol.Location = new System.Drawing.Point(344, 106);
+            this.btnDivisionSymbol.Margin = new System.Windows.Forms.Padding(6);
+            this.btnDivisionSymbol.Name = "btnDivisionSymbol";
+            this.btnDivisionSymbol.Size = new System.Drawing.Size(100, 92);
+            this.btnDivisionSymbol.TabIndex = 14;
+            this.btnDivisionSymbol.Text = "÷";
+            this.btnDivisionSymbol.UseVisualStyleBackColor = true;
+            this.btnDivisionSymbol.Click += new System.EventHandler(this.BtnOperationClick);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(456, 210);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(100, 92);
+            this.buttonClear.TabIndex = 16;
+            this.buttonClear.Text = "C";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.BtnClearClick);
+            // 
+            // btnClearEntry
+            // 
+            this.btnClearEntry.Location = new System.Drawing.Point(456, 106);
+            this.btnClearEntry.Margin = new System.Windows.Forms.Padding(6);
+            this.btnClearEntry.Name = "btnClearEntry";
+            this.btnClearEntry.Size = new System.Drawing.Size(100, 92);
+            this.btnClearEntry.TabIndex = 17;
+            this.btnClearEntry.Text = "CE";
+            this.btnClearEntry.UseVisualStyleBackColor = true;
+            this.btnClearEntry.Click += new System.EventHandler(this.BtnCEClick);
+            // 
+            // btnDecimalPoint
+            // 
+            this.btnDecimalPoint.Location = new System.Drawing.Point(232, 418);
+            this.btnDecimalPoint.Margin = new System.Windows.Forms.Padding(6);
+            this.btnDecimalPoint.Name = "btnDecimalPoint";
+            this.btnDecimalPoint.Size = new System.Drawing.Size(100, 92);
+            this.btnDecimalPoint.TabIndex = 18;
+            this.btnDecimalPoint.Text = ".";
+            this.btnDecimalPoint.UseVisualStyleBackColor = true;
+            this.btnDecimalPoint.Click += new System.EventHandler(this.BtnClick);
+            // 
+            // textboxResult
+            // 
+            this.textboxResult.Location = new System.Drawing.Point(12, 58);
+            this.textboxResult.Multiline = true;
+            this.textboxResult.Name = "textboxResult";
+            this.textboxResult.Size = new System.Drawing.Size(548, 33);
+            this.textboxResult.TabIndex = 19;
+            this.textboxResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // currentSum
+            // 
+            this.currentSum.AutoSize = true;
+            this.currentSum.Location = new System.Drawing.Point(8, 31);
+            this.currentSum.Name = "currentSum";
+            this.currentSum.Size = new System.Drawing.Size(0, 24);
+            this.currentSum.TabIndex = 20;
+            // 
+            // btnNegativeOrPositiveToggle
+            // 
+            this.btnNegativeOrPositiveToggle.Location = new System.Drawing.Point(8, 418);
+            this.btnNegativeOrPositiveToggle.Margin = new System.Windows.Forms.Padding(6);
+            this.btnNegativeOrPositiveToggle.Name = "btnNegativeOrPositiveToggle";
+            this.btnNegativeOrPositiveToggle.Size = new System.Drawing.Size(212, 92);
+            this.btnNegativeOrPositiveToggle.TabIndex = 21;
+            this.btnNegativeOrPositiveToggle.Text = "+/-";
+            this.btnNegativeOrPositiveToggle.UseVisualStyleBackColor = true;
+            this.btnNegativeOrPositiveToggle.Click += new System.EventHandler(this.BtnToggleSwitch);
+            // 
+            // calculatorApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 707);
+            this.Controls.Add(this.btnNegativeOrPositiveToggle);
+            this.Controls.Add(this.currentSum);
+            this.Controls.Add(this.textboxResult);
+            this.Controls.Add(this.btnDecimalPoint);
+            this.Controls.Add(this.btnClearEntry);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.btnDivisionSymbol);
+            this.Controls.Add(this.btnNumber1);
+            this.Controls.Add(this.btnMultiplicationSymbol);
+            this.Controls.Add(this.btnPlusSymbol);
+            this.Controls.Add(this.btnEqualsSymbol);
+            this.Controls.Add(this.btnNumber0);
+            this.Controls.Add(this.btnNumber2);
+            this.Controls.Add(this.btnNumber3);
+            this.Controls.Add(this.btnNumber6);
+            this.Controls.Add(this.btnNumber5);
+            this.Controls.Add(this.btnNumber4);
+            this.Controls.Add(this.btnSubtractionSymbol);
+            this.Controls.Add(this.btnNumber9);
+            this.Controls.Add(this.btnNumber8);
+            this.Controls.Add(this.btnNumber7);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "calculatorApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNumber7;
+        private System.Windows.Forms.Button btnNumber8;
+        private System.Windows.Forms.Button btnNumber9;
+        private System.Windows.Forms.Button btnSubtractionSymbol;
+        private System.Windows.Forms.Button btnNumber4;
+        private System.Windows.Forms.Button btnNumber5;
+        private System.Windows.Forms.Button btnNumber6;
+        private System.Windows.Forms.Button btnNumber3;
+        private System.Windows.Forms.Button btnNumber2;
+        private System.Windows.Forms.Button btnNumber0;
+        private System.Windows.Forms.Button btnEqualsSymbol;
+        private System.Windows.Forms.Button btnPlusSymbol;
+        private System.Windows.Forms.Button btnMultiplicationSymbol;
+        private System.Windows.Forms.Button btnNumber1;
+        private System.Windows.Forms.Button btnDivisionSymbol;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button btnClearEntry;
+        private System.Windows.Forms.Button btnDecimalPoint;
+        private System.Windows.Forms.TextBox textboxResult;
+        private System.Windows.Forms.Label currentSum;
+        private System.Windows.Forms.Button btnNegativeOrPositiveToggle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class calculatorApp : Form
+    {
+
+        private double currentValue = 0;
+
+
+        private string currentOperator = "";
+        public calculatorApp()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void BtnClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            textboxResult.Text += button.Text;
+
+            UpdateCurrentSum(button.Text);
+        }
+
+
+        private void BtnOperationClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            Console.WriteLine($"BtnOperationClick: currentOperator={currentOperator}, currentValue={currentValue}, textboxResult={textboxResult.Text}");
+
+            if (!string.IsNullOrEmpty(currentOperator) && !string.IsNullOrEmpty(textboxResult.Text))
+            {
+                double secondValue = double.Parse(textboxResult.Text);
+                double result = CalculateResult(currentValue, secondValue, currentOperator);
+
+                Console.WriteLine($"Result before operation: {result}");
+
+                textboxResult.Text = result.ToString();
+
+                currentOperator = "";
+                currentValue = result;
+            }
+
+            if (!string.IsNullOrEmpty(textboxResult.Text))
+            {
+                currentValue = double.Parse(textboxResult.Text);
+            }
+
+            if (button.Text == "X" && textboxResult.Text.StartsWith("-"))
+            {
+                currentValue = -Math.Abs(currentValue);
+                textboxResult.Text = currentValue.ToString();
+                Console.WriteLine($"Adjusted value for multiplication: {currentValue}");
+            }
+
+            currentOperator = button.Text;
+
+            UpdateCurrentSum(currentOperator);
+
+            textboxResult.Text = "";
+
+            Console.WriteLine($"BtnOperationClick: Updated values - currentOperator={currentOperator}, currentValue={currentValue}, textboxResult={textboxResult.Text}");
+        }
+
+        private void BtnEqualsClick(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(currentOperator) && !double.IsNaN(currentValue))
+            {
+                if (!string.IsNullOrEmpty(textboxResult.Text))
+                {
+
+
+                    double secondValue = double.Parse(textboxResult.Text);
+                    double result = CalculateResult(currentValue, secondValue, currentOperator);
+
+                    textboxResult.Text = result.ToString();
+
+                    currentOperator = "";
+                    currentValue = result;
+                    currentSum.Text = result.ToString();
+
+
+
+
+                }
+
+                else
+
+                {
+
+                    MessageBox.Show("You must enter a second value before pressing the Equals button, as you have given no second value for operation to use");
+                }
+
+            }
+        }
+
+        private void BtnClearClick(object sender, EventArgs e)
+        {
+            textboxResult.Text = "";
+            currentValue = 0;
+            currentOperator = "";
+            currentSum.Text = "";
+            currentSum.Text = "";
+        }
+
+        private void UpdateCurrentSum(string value)
+        {
+            currentSum.Text += value;
+        }
+
+
+
+        private double CalculateResult(double currentValue, double secondValue, string currentOperator)
+        {
+            switch (currentOperator)
+            {
+                case "+":
+                    return currentValue + secondValue;
+                case "-":
+                    return currentValue - secondValue;
+                case "X":
+                    return currentValue * secondValue;
+
+                case "÷":
+                    if (secondValue != 0)
+                    {
+                        return currentValue / secondValue;
+                    }
+                    else
+
+
+                    {
+                        MessageBox.Show("Cannot divide by zero", "Math Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return double.NaN;
+                    }
+                default:
+                    return double.NaN;
+            }
+        }
+
+   
+
+        private void BtnCEClick(object sender, EventArgs e)
+        {
+            textboxResult.Text = "";
+        }
+
+        private void BtnToggleSwitch(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textboxResult.Text))
+            {
+                Console.WriteLine($"Before Toggle: currentValue={currentValue}, textboxResult={textboxResult.Text}");
+
+                double parsedValue = double.Parse(textboxResult.Text);
+                parsedValue = -parsedValue;  // Toggle the sign by multiplying by -1
+
+                // Update both textboxResult and currentSum
+                textboxResult.Text = parsedValue.ToString();
+                currentSum.Text = parsedValue.ToString();  // Update currentSum as well
+
+                Console.WriteLine($"After Toggle: currentValue={currentValue}, textboxResult={textboxResult.Text}");
+            }
+        }
+
+
+
+    }
+}
